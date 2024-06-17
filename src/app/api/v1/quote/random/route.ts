c9a15456903dd0f1fa2fe2c76508d8392bc01cfc
Fractal-Tess <vgfractal@gmail.com>
2024-06-17 05:28:11 +0300

    rework random quotes api

--- src/app/api/v1/quote/random/route.ts
+++ src/app/api/v1/quote/random/route.ts
@@ -1,30 +1,12 @@
-import { db } from "@/db/index";
-import { quotes } from "@/db/schema";
+import { db } from '@/db/index';
+import { sql } from 'drizzle-orm';
 
-export const dynamic = 'force-dynamic' // defaults to auto
-export async function GET(request: Request) {
-    try {
-        // Fetch all quotes
-        const allQuotes = await db.select({
-            content: quotes.content,
-            author: quotes.author
-        }).from(quotes);
+export const dynamic = 'force-dynamic'; // defaults to auto
+export async function GET() {
+  const quote = await db.query.quotes.findFirst({
+    orderBy: sql`RAND()`,
+  });
+  if (!quote) return new Response('Error', { status: 500 });
 
-        // Select a random quote
-        if (allQuotes.length > 0) {
-            const randomIndex = Math.floor(Math.random() * allQuotes.length);
-            const randomQuote = allQuotes[randomIndex];
-
-            return new Response(JSON.stringify(randomQuote) )
-        } else {
-            return new Response("Error", {
-                status: 403
-            })
-        }
-    } catch (error) {
-        console.error("Failed to fetch quote:", error);
-        return new Response("Error" , {
-            status: 500
-        })
-    }
+  return new Response(JSON.stringify(quote));
 }
