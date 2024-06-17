fefc2fbc8025694fc29a2fb181bb10b1e8911d5d
Fractal-Tess <vgfractal@gmail.com>
2024-06-17 05:21:08 +0300

    format

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
 
+// TODO: Make these values into ENV variables
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
-  dialect: 'mysql', // 'postgresql' | 'mysql' | 'sqlite'
+  dialect: 'mysql',
   dbCredentials: {
-    host: "localhost",
-    user: "root",
-    database: "project-m",
+    host: 'localhost',
+    user: 'root',
+    database: 'project-m',
   },
 });
