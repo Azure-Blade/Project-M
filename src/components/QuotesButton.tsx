053c714e2ff6cf9814800dfb0744b62a8f9176ab
Fractal-Tess <vgfractal@gmail.com>
2024-06-17 05:34:14 +0300

    format

--- src/components/QuotesButton.tsx
+++ src/components/QuotesButton.tsx
@@ -1,30 +1,28 @@
-"use client";
+'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
 
-// Define the type for the quote object
 type Quote = {
   content: string;
   author: string;
-}
-
+};
 
 type Props = {
-  className?:string
-}
-export default function QuotesButton({className}:Props) {
+  className?: string;
+};
+export default function QuotesButton({ className }: Props) {
   const [quote, setQuote] = useState<Quote | null>(null);
 
   const fetchQuote = async () => {
     try {
-      const response = await fetch("/api/v1/quote/random");
+      const response = await fetch('/api/v1/quote/random');
       if (!response.ok) {
-        throw new Error("Network response was not ok");
+        throw new Error('Network response was not ok');
       }
       const data: Quote = await response.json();
       setQuote(data);
     } catch (error) {
-      console.error("Failed to fetch quote:", error);
+      console.error('Failed to fetch quote:', error);
     }
   };
 
@@ -38,7 +36,10 @@ export default function QuotesButton({className}:Props) {
       </button>
       {quote && (
         <div className="mt-4 p-4 glass rounded shadow">
-          <p className="text-lg italic">"{quote.content}"</p>
+          {/* When writing something like quote "", in html it is considered bad syntax (even if browsers do render it properly stay way from it!) */}
+          {/* Instead use the special characters combination in order to achieve the same result, like the &ldquo; */}
+          {/* https://www.w3.org/wiki/Common_HTML_entities_used_for_typography */}
+          <p className="text-lg italic">&ldquo;{quote.content}&ldquo;</p>
           <p className="text-right font-bold">- {quote.author}</p>
         </div>
       )}
