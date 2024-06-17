6648265c0966d8ecb86b1f2aeb4926fa64ec642b
Fractal-Tess <vgfractal@gmail.com>
2024-06-17 05:33:59 +0300

    format

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,13 @@
-import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
-import * as schema from "./schema"
+import { drizzle } from 'drizzle-orm/mysql2';
+import mysql from 'mysql2/promise';
+import * as schema from './schema';
 
+// TODO: Make these values into ENV variables
 const connection = await mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "project-m",
+  host: 'localhost',
+  user: 'root',
+  database: 'project-m',
 });
 
-export const db = drizzle(connection, {schema, mode:"default"});
-export { schema }
+export const db = drizzle(connection, { schema, mode: 'default' });
+export { schema };
