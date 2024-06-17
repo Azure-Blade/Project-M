3489955654def572a3bbde1188f30118f65bb81b
Fractal-Tess <vgfractal@gmail.com>
2024-06-17 06:40:59 +0300

    format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
         colors: {
-          text: "#dee6f8",
-          background: "#040810",
-          primary: "#6d9bfd",
-          secondary: "#831843",
-          accent: "#3c83f6",
+          text: '#dee6f8',
+          background: '#040810',
+          primary: '#6d9bfd',
+          secondary: '#831843',
+          accent: '#3c83f6',
         },
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   daisyui: {
-    themes: ["dracula", "night", "fantasy", "forest"],
+    themes: ['dracula', 'night', 'fantasy', 'forest'],
   },
 };
 export default config;
