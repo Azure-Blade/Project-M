fe5b8c9e981095e0b3a72ba0c96600a923105833
Fractal-Tess <vgfractal@gmail.com>
2024-06-17 05:33:52 +0300

    format

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,10 @@
-import {register} from "@/lib/actions/register"
+'use client';
+import { register } from '@/lib/actions/register';
 
 export default function Page() {
-
-"use client"
-
   return (
     <section className="flex-1 flex flex-col justify-center items-center h-dvh">
       <form className="flex flex-col gap-y-4 " action={register}>
-        
         <label className="input input-bordered flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -18,7 +15,12 @@ export default function Page() {
             <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
             <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
-          <input name="email" type="text" className="grow" placeholder="Email" />
+          <input
+            name="email"
+            type="text"
+            className="grow"
+            placeholder="Email"
+          />
         </label>
         <label className="input input-bordered flex items-center gap-2">
           <svg
@@ -29,7 +31,12 @@ export default function Page() {
           >
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
-          <input name="username" type="text" className="grow" placeholder="Username" />
+          <input
+            name="username"
+            type="text"
+            className="grow"
+            placeholder="Username"
+          />
         </label>
         <label className="input input-bordered flex items-center gap-2">
           <svg
@@ -44,7 +51,7 @@ export default function Page() {
               clipRule="evenodd"
             />
           </svg>
-          <input name="password" type="password" className="grow"  />
+          <input name="password" type="password" className="grow" />
         </label>
         <button type="submit" className="btn btn-primary">
           Register
